7f0437b7281bd26f91e5c4384efdbd049c9d3c30
MagnesiumSail <Leapster16@gmail.com>
2024-10-12 18:14:00 -0600

    added footer file

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <footer className='bg-plum-900 text-anti-plum-50 p-4'>
+        <h1 className='font-serif font-bold text-3xl'>I see you!</h1>
+        <h2>Don't touch my copyright!</h2>
+      </footer>
+    </>
+  );
+}
