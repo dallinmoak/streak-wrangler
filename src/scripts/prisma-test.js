a4d063977d8a22dabaa5b4217195884877dd78b9
MagnesiumSail <Leapster16@gmail.com>
2024-10-22 12:12:52 -0600

    got prisma pulling data from mongo

--- src/scripts/prisma-test.js
+++ src/scripts/prisma-test.js
@@ -1,14 +1,14 @@
 const { PrismaClient } = require('@prisma/client');
-
 const prisma = new PrismaClient();
 
 async function main() {
-  const users = await prisma.user.findMany();
-  console.log(users);
+  // Query all documents from the 'notes' collection
+  const notes = await prisma.note.findMany();
+  console.log(notes);
 }
 
 main()
-  .catch(e => console.error(e))
+  .catch((e) => console.error(e))
   .finally(async () => {
     await prisma.$disconnect();
   });
